02cc0c8bcdc1729d696d2836468adb553c2fa5ce
Fajar Pratama <fajarpratama.dev@gmail.com>
2024-05-01 13:09:13 +0700

    fix!: update user_repo

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -1,37 +1,23 @@
 package repository
 
 import (
-	"net/http"
-	"time"
-
 	"github.com/google/uuid"
 	"github.com/ngobrut/cat-tinder-api/internal/model"
-	"github.com/ngobrut/cat-tinder-api/pkg/constant"
-	"github.com/ngobrut/cat-tinder-api/pkg/custom_error"
 )
 
 // CreateUser implements IFaceRepository.
 func (r *Repository) CreateUser(data *model.User) error {
-	timestamp := time.Now().Format("2006-01-02 15:04:05")
-
 	args := []interface{}{
 		data.UserID,
 		data.Name,
 		data.Email,
 		data.Password,
-		timestamp,
-		timestamp,
+		data.CreatedAt.Format("2006-01-02 15:04:05"),
+		data.UpdatedAt.Format("2006-01-02 15:04:05"),
 	}
 
 	_, err := r.db.Exec("INSERT INTO users(user_id, name, email, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6)", args...)
 	if err != nil {
-		if IsDuplicateError(err) {
-			err = custom_error.SetCustomError(&custom_error.ErrorContext{
-				HTTPCode: http.StatusConflict,
-				Message:  constant.ErrorMessageMap[http.StatusConflict],
-			})
-		}
-
 		return err
 	}
 
@@ -63,7 +49,7 @@ func (r *Repository) FindOneUserByID(userID uuid.UUID) (*model.User, error) {
 func (r *Repository) FindOneUserByEmail(email string) (*model.User, error) {
 	res := &model.User{}
 
-	err := r.db.
+	if err := r.db.
 		QueryRow("SELECT * FROM users WHERE email = $1 AND deleted_at IS NULL", email).
 		Scan(
 			&res.UserID,
@@ -73,8 +59,7 @@ func (r *Repository) FindOneUserByEmail(email string) (*model.User, error) {
 			&res.CreatedAt,
 			&res.UpdatedAt,
 			&res.DeletedAt,
-		)
-	if err != nil {
+		); err != nil {
 		return nil, err
 	}
 
