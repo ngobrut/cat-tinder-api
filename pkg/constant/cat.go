5e205c554c77e691c8f827000cf452ee39b3bd39
Fajar Pratama <fajarpratama.dev@gmail.com>
2024-05-01 13:08:52 +0700

    feat!: cat constant validations

--- pkg/constant/cat.go
+++ pkg/constant/cat.go
@@ -1,6 +1,7 @@
 package constant
 
 type CatRace string
+type CatSex string
 
 const (
 	Persian          CatRace = "Persian"
@@ -13,20 +14,43 @@ const (
 	Abyssinian       CatRace = "Abyssinian"
 	ScottishFold     CatRace = "Scottish Fold"
 	Birman           CatRace = "Birman"
-)
 
-var CatRaces = []string{
-	string(Persian), string(MaineCoon), string(Siamese), string(Ragdoll), string(Bengal),
-	string(Sphynx), string(BritishShorthair), string(Abyssinian), string(ScottishFold), string(Birman),
-}
-
-type CatSex string
-
-const (
 	Male   CatSex = "male"
 	Female CatSex = "female"
 )
 
-var CatSexs = []string{
-	string(Male), string(Female),
+var ValidCatSex = map[string]bool{
+	string(Male):   true,
+	string(Female): true,
+}
+
+var ValidCatRace = map[string]bool{
+	string(Persian):          true,
+	string(MaineCoon):        true,
+	string(Siamese):          true,
+	string(Ragdoll):          true,
+	string(Bengal):           true,
+	string(Sphynx):           true,
+	string(BritishShorthair): true,
+	string(Abyssinian):       true,
+	string(ScottishFold):     true,
+	string(Birman):           true,
+}
+
+var CatRaces = []string{
+	string(Persian),
+	string(MaineCoon),
+	string(Siamese),
+	string(Ragdoll),
+	string(Bengal),
+	string(Sphynx),
+	string(BritishShorthair),
+	string(Abyssinian),
+	string(ScottishFold),
+	string(Birman),
+}
+
+var CatSexes = []string{
+	string(Male),
+	string(Female),
 }
