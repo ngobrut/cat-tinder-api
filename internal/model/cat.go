95dbf3690a28f91d585d9d540a7ce9b72bd1f4fc
Fajar Pratama <fajarpratama.dev@gmail.com>
2024-05-01 13:08:32 +0700

    fix!: cat_model

--- internal/model/cat.go
+++ internal/model/cat.go
@@ -15,8 +15,8 @@ type Cat struct {
 	Sex         constant.CatSex  `json:"sex" db:"sex"`
 	AgeInMonth  int              `json:"age_in_month" db:"age_in_month"`
 	Description string           `json:"description" db:"description"`
-	HasMatched  *bool            `json:"has_matched" db:"has_matched"`
-	ImageUrl    []string         `json:"image_url" db:"image_url"`
+	HasMatched  bool             `json:"has_matched" db:"has_matched"`
+	ImageURLs   []string         `json:"image_urls" db:"image_urls"`
 	CreatedAt   time.Time        `json:"created_at" db:"created_at"`
 	UpdatedAt   time.Time        `json:"updated_at" db:"updated_at"`
 	DeletedAt   *time.Time       `json:"deleted_at" db:"deleted_at"`
