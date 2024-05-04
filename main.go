b3257af48cee7f14db2ba72696d701c51c74e136
Fajar Pratama <fajarpratama.dev@gmail.com>
2024-05-04 15:23:04 +0700

    feat!: grace shutdown

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/ngobrut/cat-tinder-api/config"
 	"github.com/ngobrut/cat-tinder-api/internal/handler"
 	"github.com/ngobrut/cat-tinder-api/internal/repository"
@@ -10,30 +14,42 @@ import (
 	"github.com/ngobrut/cat-tinder-api/pkg/postgres"
 )
 
-func Exec() error {
+func Exec() (*fiber.App, error) {
 	cnf := config.New()
 
 	db, err := postgres.NewDBClient(cnf)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	repo := repository.New(db)
 	uc := usecase.New(cnf, db, repo)
 	app := handler.InitHTTPHandler(cnf, uc)
 
-	log.Printf("app running on :%d", 8080)
-	err = app.Listen(":8080")
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return app, nil
 }
 
 func main() {
-	err := Exec()
+	srv, err := Exec()
 	if err != nil {
-		log.Fatalf("[app-run-failed] \n%v\n", err)
+		log.Fatalf("[app-exec-failed] \n%v\n", err)
 	}
+
+	log.Printf("app running on :%d", 8080)
+
+	// Listen from a different goroutine
+	go func() {
+		if err := srv.Listen(":3000"); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
+
+	<-c // This blocks the main thread until an interrupt is received
+	log.Println("gracefully shutting down...")
+	_ = srv.Shutdown()
+
+	log.Println("app shutdown.")
 }
